e29463032deeaab17da0bf0aec384b75f8a1a3a6
Alvaro Cuba <alvaromcuba5@gmail.com>
2023-10-19 12:09:57 +0200

    lab :(

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,44 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(this.cards === undefined) {
+      return undefined;
+    }
+    let len = this.cards.length;
+    while (len > 0) {
+      len--;
+      let temp = this.cards[len];
+      let rdmInd = Math.floor(Math.random() * len);
+      this.cards[len] = this.cards[rdmInd];
+      this.cards[rdmInd] = temp;
+    }
+    return this.cards;
   }
+  // review 
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked ++;
+    if(card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === this.cards.length / 2) {
+      console.log("Yay you won");
+      this.shuffleCards();
+      return true;
+    } else {
+      return false;
+    }
   }
 }
